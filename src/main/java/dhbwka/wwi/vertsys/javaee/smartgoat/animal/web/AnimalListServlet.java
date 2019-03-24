3b229f7e7e95f15c1c6acfd0f3c56360cae72213
Nasi Lind <nastasiarahel@aol.com>
2019-03-24 16:49:42 +0100

    AnimalListServlet.java begonnen
    Code ist noch ausgegraut mit // da die passenden Klassen (EJBs) noch fehlen und sonst nicht richtig compiliert werden kann.

--- src/main/java/dhbwka/wwi/vertsys/javaee/smartgoat/animal/web/AnimalListServlet.java
+++ src/main/java/dhbwka/wwi/vertsys/javaee/smartgoat/animal/web/AnimalListServlet.java
@@ -21,70 +21,25 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "AnimalListServlet", urlPatterns = {"/AnimalListServlet"})
 public class AnimalListServlet extends HttpServlet {
-
+    
+    //@EJB
+    //AnimalBean animalBean;
+    
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * Liste aller Tiere anzeigen.
+     * 
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException 
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AnimalListServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AnimalListServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        //request.setAttribute("tiere", this.animalBean.findAll());
+        //request.getRequestDispatcher("/WEB-INF/list.jsp").forward(request, response);
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
